e9b4fb469fad5524a854bf2907983fe500f0d57d
Yashina Gomes <yashinagm@gmail.com>
2023-12-14 20:17:53 -0300

    Efeitos e responsividade

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const audio = document.getElementById("audio");
+    const playPauseBtn = document.getElementById("playPauseBtn");
+
+    playPauseBtn.addEventListener("click", function() {
+        if (audio.paused) {
+            audio.play();
+            playPauseBtn.innerHTML = "Pause";
+            playPauseBtn.classList.remove("play");
+            playPauseBtn.classList.add("pause");
+        } else {
+            audio.pause();
+            playPauseBtn.innerHTML = "Play";
+            playPauseBtn.classList.remove("pause");
+            playPauseBtn.classList.add("play");
+        }
+    });
+});
