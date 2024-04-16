0378973b74fbaa91890f21f4306caebdb184faab
Ana Santos <ana.br@hotmail.com>
2024-04-16 16:04:13 -0300

    inclusão do mapa e botão do topo

--- scrpits/script.js
+++ scrpits/script.js
@@ -26,3 +26,21 @@ const currentYear = currentDate.getFullYear();
 // Atualiza o elemento HTML com a data atual
 document.getElementById('copyright').textContent = `TODOS OS DIREITOS RESERVADOS DESENVOLVIDO POR CODECRAFTHOUSE | © ${currentYear}`;
 
+
+// Mostra o botão quando o usuário rolar a página
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("goTopBtn").style.display = "block";
+  } else {
+    document.getElementById("goTopBtn").style.display = "none";
+  }
+}
+
+// Função para levar o usuário de volta ao topo da página
+function topFunction() {
+  document.body.scrollTop = 0; // Para navegadores Safari
+  document.documentElement.scrollTop = 0; // Para outros navegadores
+}
+
